649a9b0e2810409c793bf741b88d4c208e9debd2
GunsWV <guguwissenbach@hotmail.com>
2025-05-07 18:18:04 +0000

    revisão: criação dqas variaveis tipo numero e texto

---
+++ Revisao.py
@@ -0,0 +1,17 @@
+#comentário
+
+#variável é um espaço na memória que receberá um dado
+#tipo : texto, número (inteiro e flutuante(moeda, altura,peso)
+
+#exemplo de número float(flutuante)
+peso = 89,5
+print(peso)
+
+#exemplo de número inteiro
+idade = 18
+print(idade)
+
+#exemplo de variável tipo texto
+nome = ("Maria")
+sobrenome = ("Silva")
+print(nome,sobrenome)
