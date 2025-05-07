70609943a4202a759768980899ce950d433b5526
GunsWV <guguwissenbach@hotmail.com>
2025-05-07 18:48:53 +0000

    exemplos de soma, subtração e multiplicação

--- Revisao.py
+++ Revisao.py
@@ -24,3 +24,15 @@ if A > B:
     print("A é maior que B")
 else:
     print("A é menor que B")
+
+#exemplo de soma
+soma = (1 + 1)
+print (soma)
+
+#exemplo subtração
+subtracao = (10 - 5)
+print(subtracao)
+
+#exemplo de multiplicação
+multiplicacao = (2 * 4)
+print("A multiplicacao de 2 * 4 é:", multiplicacao)
