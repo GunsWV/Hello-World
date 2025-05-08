14c8400be357c2118a9feadfbbe35d4520db7a78
GunsWV <guguwissenbach@hotmail.com>
2025-05-08 18:38:08 +0000

    exercicio_multiplicacao.py

---
+++ multiplicacao.py
@@ -0,0 +1,10 @@
+numero1 =int(input("Digite o primeiro numero:"))
+
+numero2 =int(input("Digite o segundo numero:"))
+
+numero3 =int(input("Digite o terceiro numero:"))
+
+numero4 =int(input("Digite o quarto numero:"))
+
+multiplicacao = numero1 * numero2 * numero3 * numero4
+print("O resultado da soma dos quatro números é : ",multiplicacao)
