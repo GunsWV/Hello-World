07ccc3314bd7fe5bec8e8889fff6071928601ed9
GunsWV <guguwissenbach@hotmail.com>
2025-05-08 18:12:44 +0000

    adicionei notas ate 4 e dividi por 4

--- Exercicio_com.py
+++ Exercicio_com.py
@@ -1,11 +1,15 @@
 nota1 =int(input('insira primeira nota:'))
 
-nota2  =float(input('insira segunda nota:'))
+nota2 =float(input('insira segunda nota:'))
+
+nota3 =int(input('insira terceira nota:'))
+
+nota4 =float(input('insira quarta nota:'))
 
 #calculo de media
-media = nota1 + nota2 / 2
+media = (nota1 + nota2 + nota3 + nota4) / 4
 
-if media >= 7:
+if media >= 5:
     print("Aprovado")
 else:
     print("Reprovado")
