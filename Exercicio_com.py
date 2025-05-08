1d5de72658963e5519ab5ad66e080e0b410d2c44
GunsWV <guguwissenbach@hotmail.com>
2025-05-08 17:55:20 +0000

    exercicio_com.py

---
+++ Exercicio_com.py
@@ -0,0 +1,12 @@
+nota1 =int(input('insira primeira nota:'))
+
+nota2  =float(input('insira segunda nota:'))
+
+#calculo de media
+media = nota1 + nota2 / 2
+
+if media >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
+    
